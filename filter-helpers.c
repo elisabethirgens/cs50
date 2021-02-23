41c4617e050f735833a2594085e5500fcd6ee05b
Elisabeth Irgens <hello@elisabethirgens.com>
2021-02-23 13:43:30 +0100

    Add partial CS50 distribution code from problem: Filter

---
+++ filter-helpers.c
@@ -0,0 +1,29 @@
+// include header files
+
+// For this problem, CS50 provides a lot of distribution code which I don't 
+// want to commit here, so this file will not compile or do much on its own
+// but it will contain the filter functions I wrote for the problem:
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
